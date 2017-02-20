80cb887faebad424971df214798bf653ee4f5115
tauomicronmu <tauomicronmurs@gmail.com>
2017-02-20 19:12:08 +0000

    got it working w/ nested lets

--- e2/evalexp.c
+++ e2/evalexp.c
@@ -11,20 +11,27 @@ struct Env {
     int    constant;
 }; 
 
+int evalexpenv(struct exp *e, struct Env *env);
+
 /* Allocates space for a new Env and returns a pointer to it */
-struct Env *newEnv() { return calloc(sizeof(struct Env), 0); } 
+struct Env *newEnv() {
+    struct Env *new = calloc(sizeof(struct Env), 1);
+    new->next     = NULL;
+    return new;
+} 
 
 /* Adds a new item to the front of an Env */
-void add(struct Env *env, char var[8], int constant) {
+struct Env *add(struct Env *env, char var[8], int constant) {
     struct Env *new = newEnv();
     strncpy(new->var, var, 8);
     new->constant = constant;
     new->next     = env;
+    return new;
 }
 
 /* Returns the value of a variable in an Env */
 int val(struct Env *env, char var[8]) {
-    if(strcmp(env->var, var)) return env->constant;
+    if(strcmp(env->var, var) == 0) return env->constant;
     if(env->next) return val(env->next, var);
     return -1;
 }
@@ -34,20 +41,20 @@ struct Env *pop(struct Env *env) { return env->next; }
 
 /* Eval an explist */
 int evalexplist(struct explist *exps, struct Env *env, enum op op) {
-    if(!exps->tail)  return evalexpenv(exps->head, env);
+    int val = evalexpenv(exps->head, env);
+    if(!exps->tail)  return val;
     if(op == ismult) return evalexpenv(exps->head, env) * evalexplist(exps->tail, env, op);
     if(op == isplus) return evalexpenv(exps->head, env) + evalexplist(exps->tail, env, op);
     return -1;
 }
 
 /* Start evalexpenv with an empty Env */
-int evalexp(struct exp *e) { return evalexpenv(e, newEnv()); }
+int evalexp(struct exp *e) { return evalexpenv(e, newEnv()); } 
 
 /* Eval an exp in a given Env */
 int evalexpenv(struct exp *e, struct Env *env) {
     if(e->tag == islet) {
-        add(env, e->bvar, evalexpenv(e->bexp, env));
-        int res = evalexpenv(e->body, env);
+        int res = evalexpenv(e->body, add(env, e->bvar, evalexpenv(e->bexp, env)));
         pop(env);
         return res; 
     }
